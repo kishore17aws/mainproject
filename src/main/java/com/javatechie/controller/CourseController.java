76cc2a6078442a5d72d471b52f84fa3252bc6155
kishore17aws <ram17kishore@gmail.com>
2024-11-20 14:27:25 +0530

    Update CourseController.java

--- src/main/java/com/javatechie/controller/CourseController.java
+++ src/main/java/com/javatechie/controller/CourseController.java
@@ -57,10 +57,7 @@ public class CourseController {
         }
     }
 
-    @GetMapping("/health")
-    public String healthCheck() {
-        return "UP";
-    }
+    
 
 
     @GetMapping("/welcome")
